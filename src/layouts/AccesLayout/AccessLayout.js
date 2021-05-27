2e33670c7aa21ac7c036aecfb2bef5b79bf69711
bcastanerc <60187468+bcastanerc@users.noreply.github.com>
2021-05-27 19:52:37 +0200

    created base configuration and files

---
+++ src/layouts/AccesLayout/AccessLayout.js
@@ -0,0 +1,7 @@
+export default {
+  name: "AccessLayout",
+  components: {},
+  data() {
+    return {};
+  }
+};
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,27 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('src/layouts/MainLayout/MainLayout.vue'),
+    path: "/",
+    component: () => import("src/layouts/MainLayout/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/Index/Index.vue') },
-      { path: 'producto/:idproducto', component: () => import('src/pages/Index/Index.vue') },
-    ],
+      { path: "", component: () => import("src/pages/Index/Index.vue") },
+      {
+        path: "producto/:idproducto",
+        component: () => import("src/pages/Index/Index.vue")
+      },
+      { path: "login", component: () => import("src/pages/Access/Access.vue") },
+      {
+        path: "register",
+        component: () => import("src/pages/Access/Access.vue")
+      }
+    ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '*',
-    component: () => import('src/pages/Error/Error404.vue'),
-  },
+    path: "*",
+    component: () => import("src/pages/Error/Error404.vue")
+  }
 ];
 
 export default routes;
