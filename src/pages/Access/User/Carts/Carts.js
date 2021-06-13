c1b6ce03e15c3a681a9dfe20508d267307fe9f58
bcastanerc <60187468+bcastanerc@users.noreply.github.com>
2021-06-13 20:54:17 +0200

    stage

---
+++ src/pages/Access/User/Carts/Carts.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'PageAboutUs',
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
---
+++ src/pages/Access/User/Carts/Carts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <q-page>
+        <div id="center">
+          <h1>Sobre PrecioJusto.app</h1>
+        </div>
+    </q-page>
+  </div>
+</template>
+
+<script src='./About.js'></script>
+<style scoped lang="scss" src="./About.scss"></style>
