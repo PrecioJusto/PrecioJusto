222d91cbe7edc36ab303b74fe36739f045902aaa
Samuel Romero Marín <samuelromeromarin@outlook.es>
2021-06-14 20:11:08 +0200

    Implemented pagination and improved size of category images

--- src/pages/Category/Categories.js
+++ src/pages/Category/Categories.js
@@ -7,22 +7,39 @@ export default {
   components: { CategoryCard, SearchBar },
   data() {
     return {
-      categories: [],
+      allCategories: [],
+      currentCategories: [],
+      current: 1,
+      max: 0,
     };
   },
   async created() {
-    this.categories = await productRepository.getCategories();
-    this.categories = this.categories.data;
+    const resp = await productRepository.getCategories();
+    const rawData = resp.data;
+    this.allCategories = rawData;
+    this.max = Math.ceil(this.allCategories.length / 16);
+
     const colors = ['green', 'red', 'blue', 'yellow', 'orange', 'purple'];
-    this.categories.forEach((category) => {
+    this.allCategories.forEach((category) => {
       category.catename = this.formatCategoryName(category.catename);
       const randomColor = colors[Math.floor(Math.random() * colors.length)];
       category.colorClass = randomColor;
     });
+    this.changePage();
+  },
+  watch: {
+    current() {
+      this.changePage();
+    },
   },
   methods: {
     formatCategoryName(str) {
       return str.charAt(0).toUpperCase() + str.slice(1).replaceAll('_', ' ');
+    },
+
+    changePage() {
+      console.log(this.current);
+      this.currentCategories = this.allCategories.slice(((this.current - 1) * 16), (((this.current - 1) * 16) + 16));
     },
   },
 };
