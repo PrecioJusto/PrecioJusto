3a11fbbaff19ede46030be1dbdad2cab163a9b45
bcastanerc <60187468+bcastanerc@users.noreply.github.com>
2021-06-14 20:34:01 +0200

    Updated routes

--- src/components/MenuBar/MenuBar.vue
+++ src/components/MenuBar/MenuBar.vue
@@ -11,10 +11,10 @@
     align="justify"
   >
     <q-tab name="Inicio" icon="las la-store-alt" label="Inicio" v-if="$q.platform.is.mobile" @click="$router.push('/').catch(()=>{})"/>
-    <q-tab name="Explora" icon="las la-stream" label="Explora" @click="$router.push('/explore').catch(()=>{})"/>
+    <q-tab name="Explora" icon="las la-stream" label="Explora" @click="$router.push('/explora').catch(()=>{})"/>
     <q-tab name="Cesta" icon="las la-shopping-cart" label="Cesta" @click="$router.push('/carrito').catch(()=>{})"/>
     <q-tab v-if="token != null" name="Cuenta" icon="lar la-user-circle" label="Cuenta" @click="user" />
-    <q-tab v-else name="Entra" icon="lar la-user-circle" label="Entra"  @click="$router.push('/login').catch(()=>{})" />
+    <q-tab v-else name="Entra" icon="lar la-user-circle" label="Entra"  @click="$router.push('/entra').catch(()=>{})" />
   </q-tabs>
  </div>
 </template>
