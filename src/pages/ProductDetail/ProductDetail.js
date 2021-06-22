6aad22d54ba9542928e9842f402e989a418fc406
bcastanerc <60187468+bcastanerc@users.noreply.github.com>
2021-06-22 12:25:59 +0200

    fix

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -30,13 +30,12 @@ export default {
       this.$q.localStorage.set('user_cart', { products: [] });
       this.cart = { products: [] };
     }
-    /* if (this.auth_token) {
-      const fauvoriteProds = awai userRepository.getFavouriteProducts();
+    if (this.auth_token) {
+      const fauvoriteProds = await userRepository.getFavouriteProducts();
       if (fauvoriteProds.data) {
-        console.log('hola');
         this.isFavourite = fauvoriteProds.data.filter((p) => p.prodid === parseInt(this.$route.params.idproduct, 10)).length !== 0;
       }
-    } */
+    }
     if (this.$route.params.idproduct) {
       const resp = await productRepository.getProduct({ prodid: this.$route.params.idproduct });
       if (resp.messageError) this.$router.push('/');
